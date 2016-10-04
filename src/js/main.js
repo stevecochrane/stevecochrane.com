23c0390843a0eb1e267be8c5863910c2d7578e11
Steve Cochrane <steve@stevecochrane.com>
2016-10-04 10:28:09 -0500

    Finished converting the main carousel script from jQuery to plain JavaScript.

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,36 @@
-// //  The Recent Work carousel script, made into a jQuery plugin just for funsies.
-// //  Applicable to any list of things that need to be rotated one by one.
-// (function($) {
-//     $.fn.carousel = function(intervalDuration) {
-//
-//         var currentListElement = 0,
-//             listElements = this.find('li'),
-//             lastListElement = listElements.length - 1,
-//             rotateListElements;
-//
-//         rotateListElements = function() {
-//             //  Check if we've reached the last list element of the group.
-//             if (currentListElement !== lastListElement) {
-//                 //  If not, increment the counter and show the next list element.
-//                 currentListElement += 1;
-//                 listElements.eq(currentListElement)
-//                     .removeClass('is-hidden-immediately')
-//                     .addClass('is-showing');
-//
-//             } else {
-//                 //  If so, hide the middle list elements (all but first and last) immediately.
-//                 //  Keep the first showing since we'll fade back to it next.
-//                 for (var i = 1; i < lastListElement; i += 1) {
-//                     listElements.eq(i)
-//                         .removeClass('is-showing')
-//                         .addClass('is-hidden-immediately');
-//                 }
-//                 //  Then fade out the last (current) list element to reveal the first.
-//                 listElements.eq(currentListElement).removeClass('is-showing');
-//                 //  Reset the counter to start the loop over from the beginning.
-//                 currentListElement = 0;
-//             }
-//         };
-//
-//         //  Call the above rotateListElements function on the specified time interval.
-//         setInterval(rotateListElements, intervalDuration);
-//         //  Set up for jQuery chaining.
-//         return this;
-//
-//     };
-// })(jQuery);
-//
-// //  Initialize carousel on #work (index page) when the page has finished loading
-// $(function() {
-//     $('#work').carousel(5000);
-// });
-
-function Carousel(elem) {
+function Carousel(elem, intervalDuration) {
     this.elem = elem;
-    this.init();
+    this.intervalDuration = intervalDuration || 5000;
 }
 
 Carousel.prototype.init = function() {
-    console.log("Carousel.init");
+    this.currentListElement = 0;
+    this.listElements = this.elem.querySelectorAll("li");
+    this.lastListElement = this.listElements.length - 1;
+
+    setInterval(this.rotateListElements.bind(this), this.intervalDuration);
+};
+
+Carousel.prototype.rotateListElements = function() {
+    //  Check if we've reached the last list element of the group.
+    if (this.currentListElement !== this.lastListElement) {
+        //  If not, increment the counter and show the next list element.
+        this.currentListElement++;
+        this.listElements[this.currentListElement].classList.remove("is-hidden-immediately");
+        this.listElements[this.currentListElement].classList.add("is-showing");
+
+    } else {
+        //  If so, hide the middle list elements (all but first and last) immediately.
+        //  Keep the first showing since we'll fade back to it next.
+        for (var i = 1; i < this.lastListElement; i++) {
+            this.listElements[i].classList.remove("is-showing");
+            this.listElements[i].classList.add("is-hidden-immediately");
+        }
+        //  Then fade out the last (current) list element to reveal the first.
+        this.listElements[this.currentListElement].classList.remove("is-showing");
+        //  Reset the counter to start the loop over from the beginning.
+        this.currentListElement = 0;
+    }
 };
 
 function addLoadEvent(newFunction) {
@@ -70,5 +49,6 @@ addLoadEvent(function() {
     var carousels = document.querySelectorAll(".carousel");
     carousels.forEach(function(carousel) {
         var newCarousel = new Carousel(carousel);
+        newCarousel.init();
     });
 });
