5b0142a308177aea31b0d47192a3d3f948063e70
Steve Cochrane <steve@stevecochrane.com>
2018-03-17 12:48:14 -0500

    Swapped out LazyLoad instantiation for Lozad.

--- src/js/portfolio-main.js
+++ src/js/portfolio-main.js
@@ -1,12 +1,2 @@
-$(function() {
-	$("img.lazy")
-		.css("max-height", function() {
-			return $(this).attr("height") + "px";
-		})
-		.show()
-		.lazyload({
-			effect: "fadeIn",
-			threshold: 400
-		}
-	);
-});
+var observer = lozad();
+observer.observe();
