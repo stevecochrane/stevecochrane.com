f442ac41a38361a4dcb0d924c02003296d3c4470
Steve Cochrane <steve@stevecochrane.com>
2018-12-03 16:23:26 -0600

    Add HTML output for all Pug files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -7,11 +8,41 @@ module.exports = {
 		main: './src/js/main.js',
 		portfolio: './src/js/portfolio.js'
 	},
+	module: {
+		rules: [
+			{
+				test: /\.pug$/,
+				use: [
+					'pug-loader'
+				]
+			}
+		]
+	},
 	plugins: [
-		new CleanWebpackPlugin(['dist/js'])
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'src/views/pages/index.pug'
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'fonts/index.html',
+			template: 'src/views/pages/fonts/index.pug'
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'music/index.html',
+			template: 'src/views/pages/music/index.pug'
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'portfolio/index.html',
+			template: 'src/views/pages/portfolio/index.pug'
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'videogames/index.html',
+			template: 'src/views/pages/videogames/index.pug'
+		})
 	],
 	output: {
 		filename: '[name].[contenthash].js',
-		path: path.resolve(__dirname, 'dist/js')
+		path: path.resolve(__dirname, 'dist')
 	}
 };
