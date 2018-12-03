ae166fa6cd8ef9c8a73ff1b9e7a1e64503e6061e
Steve Cochrane <steve@stevecochrane.com>
2018-12-03 15:39:13 -0600

    Use ES6 import for Portfolio JS dependencies

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,5 +1,8 @@
-var observer = lozad(".lazy", {
-	rootMargin: "25%",
+import IntersectionObserver from 'intersection-observer';
+import Lozad from 'lozad';
+
+let observer = Lozad('.lazy', {
+	rootMargin: '25%',
 	threshold: 0
 });
 observer.observe();
