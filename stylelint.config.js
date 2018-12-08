00d66d02ccec94fde802b72210083235a103a15a
Steve Cochrane <steve@stevecochrane.com>
2018-12-07 21:12:26 -0600

    Fix Stylelint validation issues

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,17 @@
 module.exports = {
 	extends: "stylelint-config-recommended",
-	ignoreFiles: "src/css/normalize.css"
+	rules: {
+		"at-rule-no-unknown": [
+			true,
+			{
+				"ignoreAtRules": [
+					"import-normalize"
+				]
+			}
+		],
+		//	Disabled for too many false positives with <a> element pseudoclasses,
+		//	though once portfolio.css is improved to use class selectors instead of
+		//	using so many element selectors, this problem should go away.
+		"no-descending-specificity": null
+	}
 };
