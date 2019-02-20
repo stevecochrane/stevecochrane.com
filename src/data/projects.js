bbd43ca3ad235aad07e319f22baab6b302da6cca
Steve Cochrane <steve@stevecochrane.com>
2019-02-19 22:27:57 -0600

    Convert remaining projects from Pug to JSON

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,31 @@
+const andyWowhol = require("./projects/andy-wowhol.json");
 const blizzcon2018VirtualTicket = require("./projects/blizzcon-2018-virtual-ticket.json");
+const diablo20thAnniversary = require("./projects/diablo-20th-anniversary");
+const htmlReferenceWebApplication = require("./projects/html-reference-web-application.json");
+const pixelArtScrapbook = require("./projects/pixel-art-scrapbook.json");
+const riseOfTheNecromancer = require("./projects/rise-of-the-necromancer.json");
+const vidcasterThemeEditor = require("./projects/vidcaster-theme-editor.json");
+const vidcasterThemes = require("./projects/vidcaster-themes.json");
+const vidcasterVideoPlayer = require("./projects/vidcaster-video-player.json");
+const vidcasterWebsite = require("./projects/vidcaster-website.json");
+const wowGarrisons = require("./projects/wow-garrisons.json");
+const wowLegion = require("./projects/wow-legion.json");
+const wowRedesign = require("./projects/wow-redesign.json");
 
 module.exports = {
-	projects: [blizzcon2018VirtualTicket]
+	projects: [
+		blizzcon2018VirtualTicket,
+		riseOfTheNecromancer,
+		wowRedesign,
+		diablo20thAnniversary,
+		htmlReferenceWebApplication,
+		wowLegion,
+		andyWowhol,
+		wowGarrisons,
+		pixelArtScrapbook,
+		vidcasterThemeEditor,
+		vidcasterWebsite,
+		vidcasterVideoPlayer,
+		vidcasterThemes
+	]
 };
