b28c8747b6dbbdc26e2d2b861af8ca96916cd703
Steve Cochrane <steve@stevecochrane.com>
2022-07-12 22:13:23 -0500

    Include some minified, inlined CSS in the index template

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
+const CleanCSS = require("clean-css");
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/img");
 	eleventyConfig.addPassthroughCopy("src/webfonts");
+
+	eleventyConfig.addFilter("cssmin", function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
 
 	return {
 		dir: {
