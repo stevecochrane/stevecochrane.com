5e451f19f763eee7d99e69de9463e91a40ed278e
Steve Cochrane <steve@stevecochrane.com>
2022-07-14 22:28:58 -0500

    Add a functional postcss filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,28 @@
-const CleanCSS = require("clean-css");
+const path = require("path");
+
+const cssnano = require("cssnano");
+const postcss = require("postcss");
+const postcssPresetEnv = require("postcss-preset-env");
+const postcssNormalize = require("postcss-normalize");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/img");
 	eleventyConfig.addPassthroughCopy("src/webfonts");
 
-	eleventyConfig.addFilter("cssmin", function (code) {
-		return new CleanCSS({}).minify(code).styles;
-	});
-
-	eleventyConfig.addPairedShortcode("cssPostProcess", function (code) {
-		return code;
+	eleventyConfig.addNunjucksAsyncFilter("postcss", function (code, callback) {
+		const filepath = path.join(__dirname, "./src/css/main.css");
+		postcss([
+			postcssPresetEnv({
+				stage: 2,
+				features: {
+					"nesting-rules": true,
+				},
+			}),
+			postcssNormalize(),
+			cssnano(),
+		])
+			.process(code, { from: filepath })
+			.then((result) => callback(null, result.css));
 	});
 
 	return {
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,11 @@
 module.exports = {
 	extends: "stylelint-config-recommended",
+	rules: {
+		"at-rule-no-unknown": [
+			true,
+			{
+				ignoreAtRules: ["import-normalize"],
+			},
+		],
+	},
 };
